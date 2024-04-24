6b7eaecc2b51918f728da4c6bfd475449fbf1bc9
Fabian <44522183+fabiidev@users.noreply.github.com>
2024-04-24 21:19:36 +0200

    init

---
+++ src/routes/+page.svelte
@@ -0,0 +1,86 @@
+<script>
+    import image_portfolio_david_bitterlich from '$lib/images/portfolio/david-bitterlich.jpg';
+    import image_portfolio_fsm from '$lib/images/portfolio/fsm.jpg';
+    import image_portfolio_ghf from '$lib/images/portfolio/ghf.jpg';
+
+    const date = new Date();
+</script>
+
+<section class="h-screen flex items-center justify-center">
+    <div class="flex justify-center">
+        <div class="flex flex-col">
+            <h1 class="sm:text-6xl text-5xl font-bold mb-6">
+                Hey, <br>
+                ich bin Fabian
+            </h1>
+            <div class="flex gap-4">
+                <button class="btn btn-primary">Finde mehr heraus</button>
+                <button class="btn btn-neutral">Projekte</button>
+            </div>
+        </div>
+    </div>
+</section>
+<section class="min-h-72 bg-base-300/100 pt-24 pb-24">
+    <div class="container mx-auto flex flex-col gap-6">
+        <div class="flex justify-center">
+            <p class="uppercase text-md font-bold border-2 inline-block px-4 py-1 rounded-full text-primary border-primary">
+                Portfolio</p>
+        </div>
+        <div class="flex flex-wrap gap-7 justify-center">
+            <div class="carousel rounded-box object-cover w-[20rem] h-[11rem] sm:w-[30rem] sm:h-[17rem]">
+                <div class="carousel-item w-full">
+                    <img src="{ image_portfolio_david_bitterlich }" class="w-full object-cover"
+                         alt="Tailwind CSS Carousel component"/>
+                </div>
+            </div>
+            <div class="carousel rounded-box object-contain w-[20rem] h-[11rem] sm:w-[30rem] sm:h-[17rem]">
+                <div class="carousel-item w-full">
+                    <img src="{ image_portfolio_fsm }" class="w-full object-cover"
+                         alt="Tailwind CSS Carousel component"/>
+                </div>
+            </div>
+            <div class="carousel rounded-box object-contain w-[20rem] h-[11rem] sm:w-[30rem] sm:h-[17rem]">
+                <div class="carousel-item w-full">
+                    <img src="{ image_portfolio_ghf }" class="w-full object-cover"
+                         alt="Tailwind CSS Carousel component"/>
+                </div>
+            </div>
+            <div class="carousel rounded-box object-cover w-[20rem] h-[11rem] sm:w-[30rem] sm:h-[17rem]">
+                <div class="carousel-item w-full">
+                    <img src="{ image_portfolio_david_bitterlich }" class="w-full object-cover"
+                         alt="Tailwind CSS Carousel component"/>
+                </div>
+            </div>
+            <div class="carousel rounded-box object-contain w-[20rem] h-[11rem] sm:w-[30rem] sm:h-[17rem]">
+                <div class="carousel-item w-full">
+                    <img src="{ image_portfolio_fsm }" class="w-full object-cover"
+                         alt="Tailwind CSS Carousel component"/>
+                </div>
+            </div>
+            <div class="carousel rounded-box object-contain w-[20rem] h-[11rem] sm:w-[30rem] sm:h-[17rem]">
+                <div class="carousel-item w-full">
+                    <img src="{ image_portfolio_ghf }" class="w-full object-cover"
+                         alt="Tailwind CSS Carousel component"/>
+                </div>
+            </div>
+        </div>
+        <div class="flex justify-center mt-5">
+            <a href="/portfolio">
+                <button class="btn btn-primary btn-md btn-outline">Entdecke alle Projekte</button>
+            </a>
+        </div>
+    </div>
+</section>
+<section class="bg-base-300/20 pt-36 pb-36">
+    <div class="container mx-auto flex flex-col gap-6">
+        <div class="flex justify-center">
+            <p class="uppercase text-md font-bold border-2 inline-block px-4 py-1 rounded-full text-primary border-primary">
+                Über mich</p>
+        </div>
+        <div class="flex flex-col gap-4 max-w-[50rem] mx-auto text-center" id="about">
+            <p class="text-2xl">
+                Hey, ich bin Fabian. Ich bin 21 Jahre alt und mein Ziel ist es, meine Fähigkeiten weiter zu verbessern und an zukünftigen innovativen Projekten mitzuwirken.
+            </p>
+        </div>
+    </div>
+</section>
---
+++ src/routes/impressum/+page.svelte
@@ -0,0 +1,43 @@
+<div class="sm:container prose mx-auto mt-28 mb-28">
+    <h1>Impressum</h1>
+
+    <p>Fabian Pascal Thies<br/>
+        Westerstra&szlig;e 23<br/>
+        24955 Harrislee</p>
+
+    <h2>Kontakt</h2>
+    <p>Telefon: +49 172 2175619<br/>
+        E-Mail: fabian.thies@gmx.de</p>
+
+    <p>
+        Aufgrund des Kleinunternehmerstatus gem. § 19 UStG erheben wir keine Umsatzsteuer und weisen diese daher auch
+        nicht aus.
+    </p>
+
+    <h2>
+        Haftung für Inhalte
+    </h2>
+    <p>
+        Als Diensteanbieter sind wir gemäß § 7 Abs.1 TMG für eigene Inhalte auf diesen Seiten nach den allgemeinen
+        Gesetzen verantwortlich. Nach §§ 8 bis 10 TMG sind wir als Diensteanbieter jedoch nicht verpflichtet,
+        übermittelte oder gespeicherte fremde Informationen zu überwachen oder nach Umständen zu forschen, die auf eine
+        rechtswidrige Tätigkeit hinweisen. Verpflichtungen zur Entfernung oder Sperrung der Nutzung von Informationen
+        nach den allgemeinen Gesetzen bleiben hiervon unberührt. Eine diesbezügliche Haftung ist jedoch erst ab dem
+        Zeitpunkt der Kenntnis einer konkreten Rechtsverletzung möglich. Bei Bekanntwerden von entsprechenden
+        Rechtsverletzungen werden wir diese Inhalte umgehend entfernen.
+    </p>
+
+    <h2>
+        Haftung für Links
+    </h2>
+    <p>
+        Unser Angebot enthält Links zu externen Webseiten Dritter, auf deren Inhalte wir keinen Einfluss haben. Deshalb können wir für diese fremden Inhalte auch keine Gewähr übernehmen. Für die Inhalte der verlinkten Seiten ist stets der jeweilige Anbieter oder Betreiber der Seiten verantwortlich. Die verlinkten Seiten wurden zum Zeitpunkt der Verlinkung auf mögliche Rechtsverstöße überprüft. Rechtswidrige Inhalte waren zum Zeitpunkt der Verlinkung nicht erkennbar. Eine permanente inhaltliche Kontrolle der verlinkten Seiten ist jedoch ohne konkrete Anhaltspunkte einer Rechtsverletzung nicht zumutbar. Bei Bekanntwerden von Rechtsverletzungen werden wir derartige Links umgehend entfernen.
+
+
+    </p>
+
+    <h2>EU-Streitschlichtung</h2>
+    <p>Die Europ&auml;ische Kommission stellt eine Plattform zur Online-Streitbeilegung (OS) bereit: <a
+            href="https://ec.europa.eu/consumers/odr/" target="_blank" rel="noopener noreferrer">https://ec.europa.eu/consumers/odr/</a>.<br/>
+        Unsere E-Mail-Adresse finden Sie oben im Impressum.</p>
+</div>
