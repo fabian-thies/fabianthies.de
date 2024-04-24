2f06c50513188522fcbe87ef3eec435e03577c1c
Fabian <44522183+fabiidev@users.noreply.github.com>
2024-04-25 01:12:17 +0200

    feat: added error page

---
+++ src/routes/+error.svelte
@@ -0,0 +1,8 @@
+<section class="h-[80vh] flex items-center justify-center">
+    <p class="text-8xl sm:text-[10rem] font-bold">
+        404
+    </p>
+    <div class="bg-primary text-white font-bold px-2 py-0.5 text-sm sm:text-2xl rounded rotate-12 absolute mb-[-30px]">
+        Seite nicht gefunden
+    </div>
+</section>
