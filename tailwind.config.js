b65f27c47935524dde2f1bc325875f38dad34a78
winact <thies.fab@gmail.com>
2024-11-23 19:01:48 +0100

    feat: configure darkmode

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 export default {
+    darkMode: 'class',
     content: ['./src/**/*.{html,js,svelte,ts}'],
     theme: {
         extend: {
@@ -20,4 +21,3 @@ export default {
         }
     }
 }
-
