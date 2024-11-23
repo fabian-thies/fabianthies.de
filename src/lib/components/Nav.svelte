86d4e3bdf48ff067ceb49e6b1125e8de2b466fb1
winact <thies.fab@gmail.com>
2024-11-23 17:51:22 +0100

    feat: add nav component

---
+++ src/lib/components/Nav.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+    import logo from '$lib/assets/images/site-logo.svg';
+</script>
+
+<header>
+    <nav>
+        <img src={logo} alt="Site Logo">
+    </nav>
+</header>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,9 @@
 <script>
+    import Nav from "$lib/components/Nav.svelte";
     import "../app.css";
 
     let { children } = $props();
 </script>
 
+<Nav/>
 {@render children()}
