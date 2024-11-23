0b205ab2914bd37ec5bd23046ad181459d332d8c
winact <thies.fab@gmail.com>
2024-11-23 17:50:24 +0100

    feat: adjust tailwind theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,10 +3,21 @@ export default {
     content: ['./src/**/*.{html,js,svelte,ts}'],
     theme: {
         extend: {
+            colors: {
+                background: {
+                    DEFAULT: '#fffbfa',
+                    dark: '#0a0d12',
+                },
+                title: {
+                    DEFAULT: '#0a0d12',
+                    dark: '#fffbfa',
+                },
+                subtitle: {
+                    DEFAULT: '#6b7280',
+                    dark: '#9ca3af',
+                },
+            }
         }
-    },
-    plugins: [
-        require('@tailwindcss/typography')
-    ],
+    }
 }
 
