66b54554907262a97b534ea48c38ce734fe0f240
fabianthies <44522183+fabian-thies@users.noreply.github.com>
2025-03-11 02:30:37 +0100

    chore: update tailwind from v3 to v4

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -2,7 +2,7 @@
     <p class="text-8xl sm:text-[10rem] font-bold">
         404
     </p>
-    <div class="bg-primary text-white font-bold px-2 py-0.5 text-sm sm:text-2xl rounded rotate-12 absolute mb-[-30px]">
+    <div class="bg-primary text-white font-bold px-2 py-0.5 text-sm sm:text-2xl rounded-sm rotate-12 absolute mb-[-30px]">
         Seite nicht gefunden
     </div>
 </section>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -7,7 +7,7 @@
     <section class="min-h-[calc(100vh-80px)] flex items-center text-title dark:text-title-dark">
         <div class="mx-auto w-[90%] md:w-[70%] lg:w-[80%] xl:w-[55%] text-center -mt-72 space-y-8">
             <h1 class="text-8xl font-bold">
-                Hey, ich bin <span class="text-transparent bg-clip-text bg-gradient-to-br from-purple-600 to-blue-600">Fabian</span>.
+                Hey, ich bin <span class="text-transparent bg-clip-text bg-linear-to-br from-purple-600 to-blue-600">Fabian</span>.
             </h1>
             <p class="text-lg text-subtitle dark:text-subtitle-dark font-normal max-w-xl mx-auto ">
                 Passionierter Entwickler mit einem Blick für <span>Details</span> und einer Begeisterung für
